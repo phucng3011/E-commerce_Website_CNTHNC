b00b5e0a27f6724a0b7df8421a0cf27078c500ed
phucng3011 <phucnguyen3011.work@gmail.com>
2025-03-23 08:30:18 +0700

    deploy test server

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/products', require('./routes/productRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
