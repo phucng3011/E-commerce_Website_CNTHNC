167aeac28eceaf7577d84f59aec63727005ff81c
phucng3011 <phucnguyen3011.work@gmail.com>
2025-07-29 23:36:36 +0700

    feat: Update googleCallback to use environment variable for redirect URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,7 @@ const { generateToken } = require('../config/passport');
 
 const googleCallback = (req, res) => {
   const token = generateToken(req.user);
-  res.redirect(`http://localhost:3000/login?token=${token}&name=${req.user.name}&email=${req.user.email}`);
+  res.redirect(process.env.GOOGLE_LOGIN_URL || `http://localhost:3000/login?token=${token}&name=${req.user.name}&email=${req.user.email}`);
 };
 
 module.exports = { googleCallback };
