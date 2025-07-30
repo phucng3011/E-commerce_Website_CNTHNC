c7ca2d40b95fe2185819b6d2550eff668080466c
phucng3011 <phucnguyen3011.work@gmail.com>
2025-07-30 20:37:02 +0700

    feat: Refactor googleCallback to use environment variable for frontend URL

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,8 @@ const { generateToken } = require('../config/passport');
 
 const googleCallback = (req, res) => {
   const token = generateToken(req.user);
-  res.redirect(process.env.GOOGLE_LOGIN_URL || `http://localhost:3000/login?token=${token}&name=${req.user.name}&email=${req.user.email}`);
+  const baseUrl = process.env.FRONTEND_URL || 'http://localhost:3000/login';
+  res.redirect(`${baseUrl}?token=${token}&name=${req.user.name}&email=${req.user.email}`);
 };
 
 module.exports = { googleCallback };
